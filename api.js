32fa3e905fd337ac14e5161d9274d51976ff5ed6
Komarudin <komarudin2110@gmail.com>
2019-10-10 12:54:07 +0700

    Backend-Post

--- api.js
+++ api.js
@@ -12,11 +12,31 @@ const MongoClient = mongodb.MongoClient
 
 const URL = "mongodb://127.0.0.1:27017"
 const databaseName = "bdg-mongodb"
-MongoClient.connect(URL, {}, (err, client) => {
+MongoClient.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
         return console.log(err);
     }
     const db = client.db(databaseName)
+
+    // Post Data
+    app.post('/users', (req, res) => {
+        let { name, role, age } = req.body
+        db.collection('user').insertOne({ name, role, age })
+            .then((resp) => {
+                res.send({
+                    pesan: 'Data berhasil di input',
+                    response: resp
+                })
+            }).catch((err) => {
+                res.send(err)
+            })
+    })
+
+    // Get Data
+    app.get('/users', (req, res)=>{
+        
+    })
+
 })
 
 
